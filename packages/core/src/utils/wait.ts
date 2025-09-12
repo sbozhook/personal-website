196d93ccc2d25f3009e0d2af1df2c9a70cce5e93
Sem Bozhyk <bozhyk.sem@gmail.com>
2025-09-12 16:13:41 +0200

    chore: wait with animation frame

--- packages/core/src/utils/wait.ts
+++ packages/core/src/utils/wait.ts
@@ -1,10 +1,35 @@
-export async function wait(wait: number) {
-  let r = () => ({})
-  await new Promise((resolve, reject) => {
-    r = reject as typeof r
-    setTimeout(() => {
+export async function wait(wait: number, cb?: () => void) {
+  const r = () => ({})
+  let ping = 0
+  let lastTs = 0
+  const castWait = async (resolve: (value: unknown) => void, ts: number) => {
+    requestAnimationFrame((ts) => {
+      if (ts <= wait) {
+        ping = ts - lastTs
+        lastTs = ts
+        return castWait(resolve, ts)
+      }
       resolve(undefined)
-    }, 500)
+    })
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        requestAnimationFrame(() => {
+          if (ping < 50) {
+            resolve(undefined)
+            if (cb) {
+              cb()
+            }
+          }
+          else {
+            setTimeout(resolve, wait - ping)
+          }
+        })
+      }, wait)
+    })
+    castWait(resolve, wait)
+  }
+  await new Promise((resolve) => {
+    requestAnimationFrame(ts => castWait(resolve, ts))
   })
   return {
     cancel: () => r(),
